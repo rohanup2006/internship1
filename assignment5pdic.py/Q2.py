74a5199df747b88cafcea87883a3e6e94c3ba768
rohanup2006 <rohanup2006@gmail.com>
2026-06-24 22:55:19 +0530

    Delete assignment5pdic.py directory

--- assignment5pdic.py/Q2.py
+++
@@ -1,7 +0,0 @@
-t1 = (1, 2)
-t2 = (3, 4)
-t3 = (5, 6)
-
-new_tuple = t1 + t2 + t3
-
-print("Joined Tuple:", new_tuple)
--- assignment5pdic.py/Q6.py
+++
@@ -1,8 +0,0 @@
-s = {"cat", "dog", "bird", "fish"}
-
-count = 0
-
-for item in s:
-    count += 1
-
-print("Number of elements:", count)
