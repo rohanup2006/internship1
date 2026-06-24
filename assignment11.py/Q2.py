87e1cad5f4fa654a5ba68f7c05e9971da6f5fa14
rohanup2006 <rohanup2006@gmail.com>
2026-06-24 22:50:34 +0530

    Delete assignment11.py directory

--- assignment11.py/Q2.py
+++
@@ -1,8 +0,0 @@
-#Q2) Flatten a 2d numpy array into 1d array
-import numpy as np
-a2=np.array([[1,2,3],[4,5,6]])
-print("Two-dimensional array:")
-print(a2)
-flat_arr=a2.flatten()
-print("Flattened array:")
-print(flat_arr)
--- assignment11.py/Q3.py
+++
@@ -1,9 +0,0 @@
-#Q3) Reverse a numpy array 
-import numpy as np
-a1=np.array([1,3,4,4,5])
-print("Original array")
-print(a1)
-
-rev_arr=a1[::-1]
-print("\nReversed array:")
-print(rev_arr)
